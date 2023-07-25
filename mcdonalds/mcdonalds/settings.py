642e251c96458185f040cc3935e66202a938bcab
dima <shvachko319@gmail.com>
2023-07-26 00:12:08 +0300

    make it better

--- mcdonalds/mcdonalds/settings.py
+++ mcdonalds/mcdonalds/settings.py
@@ -30,7 +30,6 @@ DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = []
 
